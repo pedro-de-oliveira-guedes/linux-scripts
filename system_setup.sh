c03a88a5c9441df2008ad58e96957af8761331a6
Pe-Guedss <pedro.og2002@gmail.com>
2022-08-13 16:41:57 -0300

    Feat: Adding the group creation commands.

--- system_setup.sh
+++ system_setup.sh
@@ -15,9 +15,15 @@ SEC="sec/"
 FOLDERS="$PUBLIC $ADM $SALES $SEC"
 PRINT_FOLDERS="\"$PUBLIC\", \"$ADM\", \"$SALES\" and \"$SEC\""
 
+# Goups
+ADM_GROUP="GRP_ADM"
+SALES_GROUP="GRP_VEN"
+SEC_GROUP="GRP_SEC"
+
 
 # ============================ Script Section =================================
 cd /home
+
 
 echo "Removing the $PRINT_FOLDERS directories, in case they already exist..."
 
@@ -26,6 +32,14 @@ rm -r -f $ADM
 rm -r -f $SALES
 rm -r -f $SEC
 
+
 echo "Creating the $PRINT_FOLDERS directories..."
 
 sudo mkdir $FOLDERS
+
+
+echo "Creating the groups..."
+
+groupadd ADM_GROUP
+groupadd SALES_GROUP
+groupadd SEC_GROUP
