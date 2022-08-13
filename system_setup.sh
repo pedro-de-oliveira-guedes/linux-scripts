bed2aabd69684cd2be81b54548525e5a46b456b2
Pe-Guedss <pedro.og2002@gmail.com>
2022-08-13 16:33:58 -0300

    Fix: Fixing variable name VEN to SALES. Fix: Removing the user elevation in the script.

--- system_setup.sh
+++ system_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-echo "For this script to run properly, you need to grant root privilegies"
-su
+
+# For this script to run properly, you have to use it in root mode!
+# Just type "sudo ./system_setup.sh" and you will be fine!
 
 # Variables section
 PUBLIC="publico/"
@@ -9,7 +10,7 @@ SALES="ven/"
 SEC="sec/"
 
 FOLDERS="$PUBLIC $ADM $SALES $SEC"
-PRINT_FOLDERS="\"$PUBLIC\", \"$ADM\", \"$VEN\" and \"$SEC\""
+PRINT_FOLDERS="\"$PUBLIC\", \"$ADM\", \"$SALES\" and \"$SEC\""
 
 cd /home
 
@@ -17,7 +18,7 @@ echo "Removing the $PRINT_FOLDERS directories, in case they already exist..."
 
 rm -r -f $PUBLIC
 rm -r -f $ADM
-rm -r -f $VEN
+rm -r -f $SALES
 rm -r -f $SEC
 
 echo "Creating the $PRINT_FOLDERS directories..."
