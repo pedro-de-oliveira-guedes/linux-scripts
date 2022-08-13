da484647f1c16703bb4df65010387f9411a477d0
Pe-Guedss <pedro.og2002@gmail.com>
2022-08-13 16:30:10 -0300

    Feat: Adding user creation and deletion to the script.

--- system_setup.sh
+++ system_setup.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
+echo "For this script to run properly, you need to grant root privilegies"
+su
 
-echo "Olá Mundo!"
+# Variables section
+PUBLIC="publico/"
+ADM="adm/"
+SALES="ven/"
+SEC="sec/"
+
+FOLDERS="$PUBLIC $ADM $SALES $SEC"
+PRINT_FOLDERS="\"$PUBLIC\", \"$ADM\", \"$VEN\" and \"$SEC\""
+
+cd /home
+
+echo "Removing the $PRINT_FOLDERS directories, in case they already exist..."
+
+rm -r -f $PUBLIC
+rm -r -f $ADM
+rm -r -f $VEN
+rm -r -f $SEC
+
+echo "Creating the $PRINT_FOLDERS directories..."
+
+sudo mkdir $FOLDERS
