a0f6aff08f9f1115dcd3faa0300939bc5bcccb5b
Pe-Guedss <pedro.og2002@gmail.com>
2022-08-21 00:56:04 -0300

    Feat: Creating a database to store the connections info.

---
+++ toshiro-shibakita-DIO/database.sql
@@ -0,0 +1,8 @@
+CREATE TABLE user_data (
+    user_id int,
+    user_name varchar(50),
+    user_last_name varchar(50),
+    user_address varchar(150),
+    user_city varchar(50),
+    user_host varchar(50)
+);
